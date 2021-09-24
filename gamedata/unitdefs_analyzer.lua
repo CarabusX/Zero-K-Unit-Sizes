5ac1c336fd92c3c9f9e08997d4b9672cb225c153
Rafał Karczmarczyk <carabusx@gmail.com>
2021-09-24 20:01:01 +0200

    Add logic for analyzing values of CEG-related tags to UnitDefs Analyzer.

--- gamedata/unitdefs_analyzer.lua
+++ gamedata/unitdefs_analyzer.lua
@@ -16,6 +16,7 @@ table.sort(unitNameAndCost, function(a, b) return (a.cost < b.cost) end)
 
 local foundUnitDefTags = {
     customparams = {},
+    sfxtypes = {},
     weapons = {},
     weapondefs = {
         customparams = {},
@@ -26,12 +27,27 @@ local foundUnitDefTags = {
 
 local exampleUnitDefValues = {
     customparams = {},
+    sfxtypes = {},
     weapons = {},
     weapondefs = {
         customparams = {},
         damage = {},
     },
     featuredefs = {},
+}
+
+local uniqueUnitDefValues = {
+    sfxtypes = {
+        explosiongenerators = {},
+    },
+    weapondefs = {
+        customparams = {
+            muzzleeffectfire = {},
+            misceffectfire = {},
+        },
+        cegtag = {},
+        explosiongenerator = {},
+    },
 }
 
 local function analyzeValues (values, foundTags, exampleValues)
@@ -45,9 +61,23 @@ local function analyzeValues (values, foundTags, exampleValues)
     end
 end
 
+local function collectUniqueValues (values, tag, uniqueValues)
+    if (values and values[tag] ~= nil) then
+        local value = values[tag]
+        uniqueValues[tag][value] = true
+    end
+end
+
 for _, ud in pairs (UnitDefs) do
     analyzeValues (ud, foundUnitDefTags, exampleUnitDefValues)
     analyzeValues (ud.customparams, foundUnitDefTags.customparams, exampleUnitDefValues.customparams)
+    analyzeValues (ud.sfxtypes, foundUnitDefTags.sfxtypes, exampleUnitDefValues.sfxtypes)
+
+    if (ud.sfxtypes and ud.sfxtypes.explosiongenerators) then
+        for _, value in ipairs(ud.sfxtypes.explosiongenerators) do
+            uniqueUnitDefValues.sfxtypes.explosiongenerators[value] = true
+        end
+    end
     if (ud.weapons) then
         for key, values in pairs(ud.weapons) do
             analyzeValues (values, foundUnitDefTags.weapons, exampleUnitDefValues.weapons)
@@ -58,6 +88,11 @@ for _, ud in pairs (UnitDefs) do
             analyzeValues (values, foundUnitDefTags.weapondefs, exampleUnitDefValues.weapondefs)
             analyzeValues (values.customparams, foundUnitDefTags.weapondefs.customparams, exampleUnitDefValues.weapondefs.customparams)
             analyzeValues (values.damage, foundUnitDefTags.weapondefs.damage, exampleUnitDefValues.weapondefs.damage)
+
+            collectUniqueValues (values, "cegtag", uniqueUnitDefValues.weapondefs)
+            collectUniqueValues (values, "explosiongenerator", uniqueUnitDefValues.weapondefs)
+            collectUniqueValues (values.customparams, "muzzleeffectfire", uniqueUnitDefValues.weapondefs.customparams)
+            collectUniqueValues (values.customparams, "misceffectfire", uniqueUnitDefValues.weapondefs.customparams)
         end
     end
     if (ud.featuredefs) then
@@ -68,3 +103,4 @@ for _, ud in pairs (UnitDefs) do
 end
 
 --Spring.Utilities.TableEcho(exampleUnitDefValues, "exampleUnitDefValues")
+--Spring.Utilities.TableEcho(uniqueUnitDefValues, "uniqueUnitDefValues")
